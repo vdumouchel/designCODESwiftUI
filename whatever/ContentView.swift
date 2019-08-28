34341eb00177ab7c0fa5797ac28a8c12958e7a1e
vdumouchel <vudmouchel@me.com>
2019-08-28 11:25:47 -0400

    Initial Commit

---
+++ whatever/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  whatever
+//
+//  Created by Vincent Dumouchel on 2019-08-28.
+//  Copyright © 2019 Vincent Dumouchel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
